cb156bfc6516b57d1900d5936319f14945b0694a
lsj <lishengjiejob@163.com>
2019-07-02 16:04:49 +0800

    Initial Commit

---
+++ iOSDemo6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSDemo6
+//
+//  Created by zhuku on 2019/7/2.
+//  Copyright © 2019 zhuku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSDemo6/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  iOSDemo6
+//
+//  Created by zhuku on 2019/7/2.
+//  Copyright © 2019 zhuku. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
